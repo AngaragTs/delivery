1b4e53575e8691e213071fbbaab623ca313febd8
25LP7649 25LP7649 <25LP7649@admins-iMac.local>
2025-10-28 15:37:21 +0800

    FirstPush

---
+++ src/app/admin/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full h-full bg-[#E4E4E7] flex">
+      <div className="w-50 h-full bg-white">asdfghjkjk</div>
+    </div>
+  );
+}
